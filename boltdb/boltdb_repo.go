73e9c6f1dcdbd77631483addd862ad2242984362
Tomas Lukosius <tomas@unfuzz.com>
2019-04-03 15:02:31 +0300

    Layered implementation.

--- boltdb/boltdb_repo.go
+++ boltdb/boltdb_repo.go
@@ -1,1 +1,45 @@
 package boltdb
+
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"time"
+)
+
+var dbBucket = []byte("last_usage_time")
+
+type BoltDb func() (*bolt.DB, error)
+
+func NewBoltDb() BoltDb {
+	return func() (db *bolt.DB, e error) {
+		return bolt.Open("pr00b121-tdd.db", 0400, &bolt.Options{ReadOnly: true})
+	}
+}
+
+func (dbFn BoltDb) Write(email string, connectTime time.Time) (err error) {
+
+	return
+
+}
+
+func (dbFn BoltDb) Read(email string) (writeTime string, err error) {
+	var db *bolt.DB
+	if db, err = dbFn(); err != nil {
+		return
+	}
+	defer db.Close()
+
+	err = db.View(func(tx *bolt.Tx) error {
+		bk := tx.Bucket(dbBucket)
+
+		bs := bk.Get([]byte(email))
+		if bs == nil {
+			return fmt.Errorf("email '%s' not found", email)
+		}
+
+		tmp := string(bs)
+		writeTime = tmp
+		return nil
+	})
+	return
+}
--- service/service.go
+++ service/service.go
@@ -1,12 +1,32 @@
 package service
 
-import "time"
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/lutomas/PR00B121-TDD/boltdb"
+	"time"
+)
 
 type Repo interface {
 	Write(email string, connectTime time.Time) error
-	Read(email string) (time.Time, error)
+	Read(email string) (string, error)
 }
 
 type EmailService struct {
 	repo Repo
+}
+
+func NewEmailService() (result *EmailService) {
+	result = new(EmailService)
+	result.repo = boltdb.NewBoltDb()
+	return
+}
+
+func (service *EmailService) Process(action, email string) (err error) {
+	writeTime, err := service.repo.Read(email)
+	if err != nil {
+		return err
+	}
+
+	logrus.Infof("Read: email=%s, time=%s", email, writeTime)
+	return
 }
