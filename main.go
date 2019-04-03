4635c75d860b256db4684b42c17d69300783638c
Tomas Lukosius <tomas@unfuzz.com>
2019-04-03 14:25:24 +0300

    Email.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/boltdb/bolt"
 	"os"
+	"regexp"
 	"time"
 )
 
@@ -13,29 +14,35 @@ var dbBucket = []byte("last_usage_time")
 func main() {
 	argsWithoutProg := os.Args[1:]
 	if len(argsWithoutProg) != 2 {
-		//os.ErrExist = fmt.Errorf("Two arguments expected")
 		logrus.Errorf("Two arguments expected")
 		os.Exit(1)
 	}
 
 	db, err := bolt.Open("pr00b121-tdd.db", 0600, nil)
 	if err != nil {
-		os.ErrExist = err
 		logrus.Errorf("Failure caused by: %v", err)
 		os.Exit(1)
 	}
 	defer db.Close()
 
-	switch argsWithoutProg[0] {
+	action := argsWithoutProg[0]
+	email := argsWithoutProg[1]
+
+	matched, err := regexp.Match(`.*@.*\..*`, []byte(email))
+	if matched == false {
+		logrus.Errorf("Non email provided: '%s'", email)
+		os.Exit(1)
+	}
+
+	switch action {
 	case "read":
-		id := argsWithoutProg[1]
 		var writeTime string
 		err = db.View(func(tx *bolt.Tx) error {
 			bk := tx.Bucket(dbBucket)
 
-			bs := bk.Get([]byte(id))
+			bs := bk.Get([]byte(email))
 			if bs == nil {
-				return fmt.Errorf("not found id: %s", id)
+				return fmt.Errorf("email '%s' not found", email)
 			}
 
 			writeTime = string(bs)
@@ -43,16 +50,15 @@ func main() {
 		})
 		if err != nil {
 			os.ErrExist = err
-			logrus.Errorf("Write action failure caused by: %v", err)
+			logrus.Errorf("Read failed: %v", err)
 			os.Exit(1)
 		}
 
-		logrus.Infof("Read: id=%s, time=%s", id, writeTime)
+		logrus.Infof("Read: email=%s, time=%s", email, writeTime)
 
 	case "write":
-		id := argsWithoutProg[1]
 		writeTime := time.Now().Format(time.RFC3339)
-		logrus.Infof("Write: id=%s, time=%s", id, writeTime)
+		logrus.Infof("Write: email=%s, time=%s", email, writeTime)
 
 		err = db.Update(func(tx *bolt.Tx) error {
 			bk, err := tx.CreateBucketIfNotExists(dbBucket)
@@ -60,7 +66,7 @@ func main() {
 				return fmt.Errorf("failed to create bucket: %v", err)
 			}
 
-			if err := bk.Put([]byte(id), []byte(writeTime)); err != nil {
+			if err := bk.Put([]byte(email), []byte(writeTime)); err != nil {
 				return fmt.Errorf("failed to insert '%s': %v", writeTime, err)
 			}
 			return nil
